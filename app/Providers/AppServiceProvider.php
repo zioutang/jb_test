fe084b962155eb8dc4d59c282ae000fe92d41ddf
zioutang <tangziou@gmail.com>
2017-12-02 00:14:06 -0800

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,12 +25,12 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
       // Force SSL in production
-      // if ($this->app->environment() == 'production') {
-      // URL::forceScheme('https');
+      if ($this->app->environment() == 'production') {
+      URL::forceScheme('https');
+      }
+      // if (env('APP_ENV') === 'production') {
+      //   $this->app['request']->server->set('HTTPS', true);
       // }
-      if (env('APP_ENV') === 'production') {
-        $this->app['request']->server->set('HTTPS', true);
-}
     }
 
 }
